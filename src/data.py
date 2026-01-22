2e3f713a6a1110cf73c98605dd68e124127b6324
Sami-ul <72280649+Sami-ul@users.noreply.github.com>
2026-01-21 19:16:47 -0700

    update data.py env path

--- src/data.py
+++ src/data.py
@@ -3,5 +3,5 @@ from dotenv import set_key
 
 path = kagglehub.dataset_download("ananthu017/emotion-detection-fer")
 
-set_key(dotenv_path=".env", key_to_set="DATASET_PATH", value_to_set=path)
+set_key(dotenv_path="src/.env", key_to_set="DATASET_PATH", value_to_set=path)
 print("Set DATASET_PATH env variable to: ", path)
